1ce7e1f5a76238a5d3fb800018b7d544812ef39c
vicaqu <15585824.clot@fje.edu>
2024-01-31 11:35:05 +0000

    Commit 1 del projecte act14

---
+++ app/codi.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="ca">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Detalls de la Reserva</title>
+</head>
+<body>
+
+<?php
+$roomType = $_POST['roomType'];
+$numRooms = $_POST['numRooms'];
+$numPeople = $_POST['numPeople'];
+$numDoubleBeds = $_POST['numDoubleBeds'];
+$numSingleBeds = $_POST['numSingleBeds'];
+$numNights = $_POST['numNights'];
+$name = $_POST['name'];
+$creditCard = $_POST['creditCard'];
+
+if ($roomType == 'basic') {
+  $pricePerNight = 40;
+} else {
+  $pricePerNight = 50;
+}
+
+$pricePerDoubleBed = 15;
+$pricePerSingleBed = 10;
+
+$totalPrice = ($pricePerNight * $numPeople + $pricePerDoubleBed * $numDoubleBeds + $pricePerSingleBed * $numSingleBeds) * $numRooms * $numNights;
+
+echo "<h2>Detalls de la Reserva</h2>";
+echo "<p>Nom i cognoms: $name</p>";
+echo "<p>Número de VISA: $creditCard</p>";
+echo "<p>Preu total de la reserva: $totalPrice €</p>";
+echo '<p><a href="index.html">Tornar al Formulari</a></p>';
+?>
+
+</body>
+</html>
